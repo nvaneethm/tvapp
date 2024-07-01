64cae350b4aab2c1f22e013ca0add1e9620db462
nvaneethm <navaneethna.m@gmail.com>
2024-07-01 11:59:56 +0530

    focusable button

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ onClick, children, tabIndex }) => (
-  <button onClick={onClick} tabIndex={tabIndex}>
-    {children}
-  </button>
-);
+const Button = ({ onClick, children, tabIndex }) => {
+  const buttonRef = useRef(null);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      onClick();
+    }
+  };
+
+  useEffect(() => {
+    buttonRef.current.focus();
+  }, []);
+
+  return (
+    <button
+      ref={buttonRef}
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+      tabIndex={tabIndex}
+      style={{ padding: '10px 20px', cursor: 'pointer' }}
+    >
+      {children}
+    </button>
+  );
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
